27e6844efd4970f3bec0dc8a71724418d6af071f
sanlav89 <sanlav89@mail.ru>
2021-12-19 17:26:59 +0300

    add options from task

--- main.cpp
+++ main.cpp
@@ -1,32 +1,49 @@
 #include <boost/program_options.hpp>
 #include <iostream>
+#include <list>
 
 namespace po = boost::program_options;
 
-void set_bulk(size_t bulk) {
-    std::cout << "bulk size is " << bulk << std::endl;
-}
+using strlist_t = std::vector<std::string>;
 
 int main(int argc, const char *argv[]) {
     try {
         po::options_description desc{"Options"};
         desc.add_options()
-                ("help,h", "This screen")
-                ("config", po::value<std::string>()->default_value("app.yaml"), "config filename")
-                ("bulk", po::value<size_t>()->default_value(5)->notifier(set_bulk), "bulk size");
+                ("help,h", "Suppported commands")
+                ("incdir,i", po::value<strlist_t>()->composing()->required(), "directories for scan")
+                ("excdir,e", po::value<strlist_t>()->composing(), "excluded directories for scan")
+                ("minsize,s", po::value<size_t>()->default_value(1), "minimal size of file")
+                ("mask,m", po::value<strlist_t>()->composing(), "masks of files")
+                ("blocksize,b", po::value<size_t>()->default_value(1), "compare block size")
+                ("hashtype,t", po::value<size_t>()->default_value(0), "hash type: 0 - md5, 1 - crc32")
+                ;
 
         po::variables_map vm;
         po::store(parse_command_line(argc, argv, desc), vm);
+
+        if (vm.count("help")) {
+            std::cout << desc << '\n';
+        }
+
         po::notify(vm);
 
-        if (vm.count("help"))
-            std::cout << desc << '\n';
-        else if (vm.count("config"))
-            std::cout << "readfrom: " << vm["config"].as<std::string>() << std::endl;
-        else if (vm.count("bulk"))
-            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
-    }
-    catch (const std::exception &e) {
+//        if (vm.count("help")) {
+//            std::cout << desc << '\n';
+//        } else if (vm.count("incdir")) {
+//            std::cout << "readfrom: " << vm["config"].as<std::string>() << std::endl;
+//        } else if (vm.count("bulk")) {
+//            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
+//        } else if (vm.count("bulk")) {
+//            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
+//        } else if (vm.count("bulk")) {
+//            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
+//        } else if (vm.count("bulk")) {
+//            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
+//        } else if (vm.count("bulk")) {
+//            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
+//        }*/
+    } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
 }
