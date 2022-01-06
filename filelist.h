e85a9f0a06e334e5ce715ce0b23c8b0c860f1f83
sanlav89 <sanlav89@mail.ru>
2022-01-06 10:43:02 +0300

    function what creates the filelist

--- filelist.h
+++ filelist.h
@@ -12,122 +12,205 @@
 
 namespace fs = boost::filesystem;
 
-namespace filelist {
+namespace scan {
 
-using dirs_t = std::vector<std::string>;
-using mask_t = std::vector<std::string>;
-using paths_t = std::vector<fs::path>;
+using filenames_t = std::vector<std::string>;
+using mask_t = std::string;
+using paths_t = std::list<fs::path>;
 using filelist_t = std::list<std::string>;
 
-class FileListCreator
+enum ScanLevel {
+    Current,
+    All
+};
+
+using level_t = ScanLevel;
+
+paths_t fileList(
+        const filenames_t &included = {"./"},
+        const filenames_t &excluded = {},
+        const ScanLevel &level = Current,
+        const mask_t &mask = {}
+        )
 {
-public:
-    FileListCreator(
-            const dirs_t &includeDirs,
-            const dirs_t &excludeDirs = {}/*,
-            const mask_t &mask = {}*/
-            )
-    {
-        filterInputData(includeDirs, excludeDirs);
+    paths_t result;
+    paths_t includedPaths;
+    paths_t excludedPaths;
+
+    for (const auto &filename : included) {
+        includedPaths.push_back(fs::canonical(fs::path(filename)));
+    }
+
+    for (const auto &filename : excluded) {
+        excludedPaths.push_back(fs::canonical(fs::path(filename)));
+    }
+
+    for (auto &dir : includedPaths) {
+
+        // watch if dir is in excluded list
+        if (std::find(excludedPaths.begin(), excludedPaths.end(), dir) != excludedPaths.end()) {
+            continue;
+        }
+
+        if (level == All) {
+            for (auto &entry : fs::recursive_directory_iterator(dir)) {
+
+                // watch if entry is in excluded list
+                if (std::find(excludedPaths.begin(), excludedPaths.end(), dir) != excludedPaths.end()) {
+                    continue;
+                }
+
+                if (fs::is_regular_file(entry)) {
+                    result.push_back(entry);
+                }
+            }
+        } else {
+            for (auto &entry : fs::directory_iterator(dir)) {
+
+                if (fs::is_regular_file(entry)) {
+                    result.push_back(entry);
+                }
+
+            }
+        }
+
+    }
+
+    return result;
+}
+
+void printFileList(const paths_t &fileList, std::ostream &os = std::cout)
+{
+    for (const auto &path : fileList) {
+        os << fs::canonical(path).native() << std::endl;
+    }
+}
+
+
 /*
-        static const boost::regex e("<(From )?([A-Za-z0-9_]+)>(.*?)");
-        std::string chat_input("<Darker> Hello");
-        boost::match_results<std::string::const_iterator> results;
-        if (boost::regex_match(chat_input, results, e))
-        {
-             std::string playername = results[2];  //Get contents of the second (...)
-        }
+bool isSubString(const std::string &mainStr, const std::string &subStr)
+{
+    return mainStr.find(subStr) != std::string::npos;
+}
+
+bool isSubPath(const fs::path &mainPath, const fs::path &subPath)
+{
+    return isSubString(fs::canonical(mainPath).native(),
+                       fs::canonical(subPath).native());
+}
 */
-        for (auto &incdir : includeDirs) {
-            for (auto &entry : fs::recursive_directory_iterator(incdir)) {
 
-                if (!fs::is_regular_file(entry)) {
-                    continue;
-                }
 
-                boost::smatch what;
-
-//                if (!boost::regex_match(entry.path().filename().native(), what, boost::regex("[5]"))) {
-                if (!boost::regex_match(entry.path().filename().native(), what, boost::regex(".*\Pack.*\cmake"))) {
-                    continue;
-                }
-
-//                if (fs::is_regular_file(entry) && isFilteredByMask(entry, mask)) {
-                    std::cout << fs::canonical(entry) << std::endl;
-//                }
-            }
-        }
-    }
-
-private:
-    paths_t m_incPaths;
-    paths_t m_excPaths;
-
-    void filterInputData(const dirs_t &includeDirs, const dirs_t &excludeDirs)
-    {
-        bool same = false;
-        for (const auto &incName : includeDirs) {
-            fs::path incPath = absoluteDirPath(incName);
-            same = false;
-            for (const auto &excName : excludeDirs) {
-                fs::path excPath = absoluteDirPath(excName);
-                if (excPath.compare(incPath) == 0) {
-                    same = true;
-                    break;
-                } else if (isSubPath(incPath, excPath)) {
-                    m_excPaths.push_back(excPath);
-                }
-            }
-            if (!same) {
-                m_incPaths.push_back(incPath);
-            }
-        }
-    }
-
-    static bool isSubPath(const fs::path &mainPath, const fs::path &subPath)
-    {
-        return isSubString(mainPath.native(), subPath.native());
-    }
-
-    static bool isSubString(const std::string &mainStr, const std::string &subStr)
-    {
-        return mainStr.find(subStr) != std::string::npos;
-    }
-
-    static fs::path absoluteDirPath(const fs::path &path)
-    {
-        fs::path result = fs::canonical(path);
-        if (!fs::is_directory(path)) {
-            throw path.native() + " is not a directory!";
-        }
-        return result;
-    }
-
-    static bool isFilteredByMask(const fs::path &file, const mask_t &mask)
-    {
-        if (mask.empty()) {
-            return true;
-        }
-        for (const auto &part : mask) {
-            if (!isSubString(file.native(), part)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-//    void toAbsolutePaths(const dirs_t &paths)
+//class FileListCreator
+//{
+//public:
+//    FileListCreator(
+//            const dirs_t &includeDirs,
+//            const dirs_t &excludeDirs = {}/*,
+//            const mask_t &mask = {}*/
+//            )
 //    {
-//        dirs_t result;
-//        for (const auto &)
+//        filterInputData(includeDirs, excludeDirs);
+///*
+//        static const boost::regex e("<(From )?([A-Za-z0-9_]+)>(.*?)");
+//        std::string chat_input("<Darker> Hello");
+//        boost::match_results<std::string::const_iterator> results;
+//        if (boost::regex_match(chat_input, results, e))
+//        {
+//             std::string playername = results[2];  //Get contents of the second (...)
+//        }
+//*/
+//        for (auto &incdir : includeDirs) {
+//            for (auto &entry : fs::recursive_directory_iterator(incdir)) {
+
+//                if (!fs::is_regular_file(entry)) {
+//                    continue;
+//                }
+
+//                boost::smatch what;
+
+////                if (!boost::regex_match(entry.path().filename().native(), what, boost::regex("[5]"))) {
+//                if (!boost::regex_match(entry.path().filename().native(), what,
+//                                        boost::regex(".*\pack.*\cmake", boost::regex::icase))) {
+//                    continue;
+//                }
+
+////                if (fs::is_regular_file(entry) && isFilteredByMask(entry, mask)) {
+//                    std::cout << fs::canonical(entry) << std::endl;
+////                }
+//            }
+//        }
+//    }
+
+//private:
+//    paths_t m_incPaths;
+//    paths_t m_excPaths;
+
+//    void filterInputData(const dirs_t &includeDirs, const dirs_t &excludeDirs)
+//    {
+//        bool same = false;
+//        for (const auto &incName : includeDirs) {
+//            fs::path incPath = absoluteDirPath(incName);
+//            same = false;
+//            for (const auto &excName : excludeDirs) {
+//                fs::path excPath = absoluteDirPath(excName);
+//                if (excPath.compare(incPath) == 0) {
+//                    same = true;
+//                    break;
+//                } else if (isSubPath(incPath, excPath)) {
+//                    m_excPaths.push_back(excPath);
+//                }
+//            }
+//            if (!same) {
+//                m_incPaths.push_back(incPath);
+//            }
+//        }
+//    }
+
+//    static bool isSubPath(const fs::path &mainPath, const fs::path &subPath)
+//    {
+//        return isSubString(mainPath.native(), subPath.native());
+//    }
+
+//    static bool isSubString(const std::string &mainStr, const std::string &subStr)
+//    {
+//        return mainStr.find(subStr) != std::string::npos;
+//    }
+
+//    static fs::path absoluteDirPath(const fs::path &path)
+//    {
+//        fs::path result = fs::canonical(path);
+//        if (!fs::is_directory(path)) {
+//            throw path.native() + " is not a directory!";
+//        }
 //        return result;
 //    }
 
-//    static bool is_subdirectory()
+//    static bool isFilteredByMask(const fs::path &file, const mask_t &mask)
+//    {
+//        if (mask.empty()) {
+//            return true;
+//        }
+//        for (const auto &part : mask) {
+//            if (!isSubString(file.native(), part)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
-//    filelist_t m_filelist;
+////    void toAbsolutePaths(const dirs_t &paths)
+////    {
+////        dirs_t result;
+////        for (const auto &)
+////        return result;
+////    }
+
+////    static bool is_subdirectory()
+
+////    filelist_t m_filelist;
 
 
-};
+//};
 
 }
--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include <boost/program_options.hpp>
 #include <iostream>
 #include <list>
+#include <set>
+#include <algorithm>
 
 #include "filelist.h"
 
 namespace po = boost::program_options;
 
-using strlist_t = std::vector<std::string>;
-
 int main(int argc, const char *argv[]) {
+
     try {
-        filelist::dirs_t dirs;
+        scan::filenames_t included, excluded;
+        size_t level;
+
         po::options_description desc{"Options"};
         desc.add_options()
                 ("help,h", "Suppported commands")
-                ("incdir,i", po::value<strlist_t>(&dirs)->composing()->required(), "directories for scan")
-                ("excdir,e", po::value<strlist_t>()->composing(), "excluded directories for scan")
+                ("incdir,i", po::value<scan::filenames_t>(&included)->composing(), "directories for scan")
+                ("excdir,e", po::value<scan::filenames_t>(&excluded)->composing(), "excluded directories for scan")
+                ("level,l", po::value<size_t>(&level)->composing(), "scan level: 1 - recursive, 0 - current directory only")
                 ("minsize,s", po::value<size_t>()->default_value(1), "minimal size of file")
-                ("mask,m", po::value<strlist_t>()->composing(), "masks of files")
+                ("mask,m", po::value<scan::mask_t>()->composing(), "masks of files")
                 ("blocksize,b", po::value<size_t>()->default_value(1), "compare block size")
                 ("hashtype,t", po::value<size_t>()->default_value(0), "hash type: 0 - md5, 1 - crc32")
                 ;
@@ -30,8 +34,9 @@ int main(int argc, const char *argv[]) {
         }
 
         po::notify(vm);
+        scan::printFileList(scan::fileList(included, excluded, static_cast<scan::level_t>(level)));
 
-        filelist::FileListCreator fl(dirs);
+//        filelist::FileListCreator fl(dirs);
 
 //        if (vm.count("help")) {
 //            std::cout << desc << '\n';
@@ -47,8 +52,9 @@ int main(int argc, const char *argv[]) {
 //            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
 //        } else if (vm.count("bulk")) {
 //            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
-//        }*/
+//        }
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
+
 }
