ce43bc23d472dc83c548d402ca74f992dbf6c8e5
sanlav89 <sanlav89@mail.ru>
2022-01-22 17:45:48 +0300

    Revert "remove files for review"
    This reverts commit f711407207a819b0cd373693647056668588aff9.

---
+++ filesfilter.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <boost/regex.hpp>
+#include <boost/algorithm/string.hpp>
+#include "hash.h"
+
+namespace fs = boost::filesystem;
+
+namespace filesfilter {
+
+using filenames_t = std::vector<std::string>;
+using path_t = fs::path;
+using pathconteiner_t = std::list<path_t>;
+using mask_t = std::string;
+
+enum ScanLevel {
+    Current,
+    All
+};
+
+using level_t = ScanLevel;
+
+namespace def {
+const filenames_t Included = {"./"};
+const filenames_t Excluded = {};
+const level_t Level = Current;
+const mask_t Mask = ".*";
+const size_t Minsize = 1;
+const size_t Blocksize = 1;
+const hash::alg_t Alg = hash::AlgMd5;
+}
+
+class SameFilesFinder
+{
+public:
+
+    /**
+     * @brief При создании SameFilesFinder выполняется алгоритм поиска дубликатов, который состоит из двух этапов:
+     * 1. Формирование списка файлов для сравнения (createFileList()).
+     * Алгоритм createFileList():<br>
+     * 1) Преобразование списков included, excluded в формат boost::filesystem::path (path_t).<br>
+     * 2) Проход по всем included. В зависимости от level - рекурсивный проход (с вхождениями во вложенные директории), либо нерекурсивный (про текущей директории).<br>
+     * 3) Фильтр каждого вхождения по критериям:
+     *    - вхождение является файлом;
+     *    - размер файла не менее minsize;
+     *    - имя файла сочетсатся с маской mask.
+     * В итоге формируется список файлов, прошедших все фильтры, который можно получить при помощи метода fileList() или вывести в поток при помощи printFileList().<br>
+     * 2. Формирование групп файлов-дубликатов (findSameFiles())
+     * Алгоритм (findSameFiles()):<br>
+     * 1) Если количество файлов <= 1, досточный выход из функции, так как снавнивать не с чем. Выполняется требование ТЗ: блок файла читается только в случае необходимости.<br>
+     * 2) Формируется ассоциативный массив, где ключом является хэш прочитанного из файла блока, а значением - список файлов, для которых данное хэш-значение одинаково.<br>
+     * 3) Формируется список файлов, чтение которых завершено. Если таких файлов оказалось более 1, то это значит, что все предыдущие блоки для них одинаковы, и их можно объединить в группу и записать в итоговый результат.<br>
+     * 4) Для всех списков файлов из ассоциативного массива, размером больше 1 вызываем функцию рекурсивно.<br>
+     * В итоге формируется список групп файлов-дубликатов, который можно получить при помощи метода sameFilesGroups() или вывести в поток при помощи printSameFiles().<br>
+     * @param included - список директорий для сканирования
+     * @param excluded - список директорий для исключения из сканирования
+     * @param mask - маска сканирования
+     * @param level - уровень сканирования
+     * @param minsize - минимальный размер файлов
+     * @param blocksize - размер блока, по которому производится четние каждого файла
+     * @param hashalg - алгоритм хэширования читаемого из файла блока: 0 - MD5, 1 - CRC32
+     */
+    SameFilesFinder(
+            const filenames_t &included,
+            const filenames_t &excluded,
+            const mask_t &mask,
+            const level_t &level,
+            const size_t &minsize,
+            const size_t &blocksize,
+            const hash::alg_t &hashalg
+            );
+
+    void printFileList(std::ostream &os = std::cout);
+    void printSameFiles(std::ostream &os = std::cout);
+
+    pathconteiner_t fileList() const;
+
+    std::list<pathconteiner_t> sameFilesGroups() const;
+
+private:
+
+    pathconteiner_t m_fileList;
+    std::list<pathconteiner_t> m_sameFilesGroups;
+    size_t m_blocksize;
+    hash::alg_t m_hashalg;
+
+    /**
+     * @brief Формирование списка файлов для сравнения
+     * @param included - список директорий для сканирования
+     * @param excluded - список директорий для исключения из сканирования
+     * @param mask - маска сканирования
+     * @param level - уровень сканирования
+     * @param minsize - минимальный размер файлов
+     */
+    void createFileList(
+            const filenames_t &included,
+            const filenames_t &excluded,
+            const mask_t &mask,
+            const level_t &level,
+            const size_t &minsize
+            );
+
+    bool contains(
+            const pathconteiner_t &pathContainer,
+            const path_t &pathEntry
+            );
+
+    bool filtered(
+            const path_t &pathEntry,
+            const mask_t &mask,
+            const size_t &minsize
+            );
+
+    /**
+     * @brief Формирование групп файлов-дубликатов.
+     * @param filelist - список файлов, подлежащих сравнению
+     * @param blockNumber - номер блока, который необходимо прочитать из каждого файла из списка
+     */
+    void findSameFiles(const pathconteiner_t &filelist, size_t blockNumber);
+
+    bool canCreateHash(const path_t &filename, size_t blockNumber);
+
+    hash::Hash hashOfFileBlock(const path_t &filename, size_t blockNumber);
+
+    static void printPathContainer(
+            const pathconteiner_t &pathContainer,
+            std::ostream &os = std::cout
+            );
+
+};
+
+}
---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <boost/program_options.hpp>
+#include "filesfilter.h"
+
+namespace po = boost::program_options;
+namespace ff = filesfilter;
+namespace def = ff::def;
+
+// Windows run example
+// D:\WORK\OTUS\build-otus_homework_07-Desktop_Qt_5_15_0_MinGW_64_bit-Debug>bayan -i ../otus_homework_07/_test_folder/ -l 1
+int main(int argc, const char *argv[])
+{
+
+    try {
+
+        ff::filenames_t included, excluded;
+        ff::mask_t mask;
+        size_t level, minsize, blocksize, hashtype;
+
+        po::options_description desc{"Options"};
+        desc.add_options()
+                ("help,h", "Suppported commands")
+                ("incdir,i", po::value<ff::filenames_t>(&included)->composing(), "directories for scan")
+                ("excdir,e", po::value<ff::filenames_t>(&excluded)->composing(), "excluded directories for scan")
+                ("level,l", po::value<size_t>(&level)->default_value(def::Level), "scan level: 1 - recursive, 0 - current directory only")
+                ("mask,m", po::value<ff::mask_t>(&mask)->composing()->default_value(def::Mask), "regex: masks of files")
+                ("minsize,s", po::value<size_t>(&minsize)->default_value(def::Minsize), "minimal size of file")
+                ("blocksize,b", po::value<size_t>(&blocksize)->default_value(def::Blocksize), "compare block size")
+                ("hashtype,t", po::value<size_t>(&hashtype)->default_value(def::Alg), "hash type: 0 - md5, 1 - crc32")
+                ;
+
+        po::variables_map vm;
+        po::store(parse_command_line(argc, argv, desc), vm);
+
+        if (vm.count("help")) {
+            std::cout << desc << '\n';
+        } else {
+
+            po::notify(vm);
+
+            if (included.empty()) {
+                included = def::Included;
+            }
+
+            if (blocksize < def::Blocksize) {
+                blocksize = def::Blocksize;
+            }
+
+            ff::SameFilesFinder scanner(
+                        included, excluded, mask, static_cast<ff::level_t>(level),
+                        minsize, blocksize, static_cast<hash::alg_t>(hashtype)
+                        );
+
+//            scanner.printFileList();
+            scanner.printSameFiles();
+
+        }
+
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+}
